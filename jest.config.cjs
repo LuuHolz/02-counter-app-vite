d86914cf38c1300f7b6f041605a43ae140f3aa9e
Luciana <luciana.holzmann@gmail.com>
2024-06-08 00:11:03 +0200

    [DOC] Configuraciones y dependencias

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    globals: {
-      'ts-jest': {
-        tsconfig: 'tsconfig.json', 
-      },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+      esModuleInterop: true, 
     },
-  };
-
-  
+  },
+  setupFiles: ['./jest.setup.js']
+};
